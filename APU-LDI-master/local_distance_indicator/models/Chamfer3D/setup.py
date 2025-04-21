7ece3c94db18db8a4de72acdb38a72860f0429df
Philip Root <proot@ND.EDU>
2025-04-20 23:33:16 -0400

    stuff

--- APU-LDI-master/local_distance_indicator/models/Chamfer3D/setup.py
+++ APU-LDI-master/local_distance_indicator/models/Chamfer3D/setup.py
@@ -1,5 +1,9 @@
-from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from setuptools import setup
+
+import torch
+print(torch.cuda.is_available())  # Should be True
+print(torch.version.cuda)         # Should say 12.1
 
 setup(
     name='chamfer_3D',
